ad0dfafcd56e8f0f8317f7d5edc9b88b64469fd0
Hector Martinez Espinosa <mtzehector@gmail.com>
2025-10-31 21:34:43 -0600

    Actualizaci├│n inicial del proyecto

--- src/main/java/mx/gob/imss/dpes/capacidadcredito/exception/SelloElectronicoException.java
+++
@@ -1,9 +0,0 @@
-package mx.gob.imss.dpes.capacidadcredito.exception;
-
-import mx.gob.imss.dpes.common.exception.BusinessException;
-
-public class SelloElectronicoException extends BusinessException {
-  private static final String KEY = "err006";
-
-  public SelloElectronicoException() {super(KEY);}
-}
